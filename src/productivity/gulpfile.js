db25c6f40216c211abd6b3b55fd9b433d83cdeec
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-02-03 23:27:33 +0100

    :bug: Fix local mocha path

--- src/productivity/gulpfile.js
+++ src/productivity/gulpfile.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec;
 const gulp = require('gulp');
 
 gulp.task('test', (done) => {
-  exec('node ./node_modules/mocha/bin/mocha --compilers js:babel-core/register ./test', (error, stdout, stderr) => {
+  exec('./node_modules/mocha/bin/mocha --compilers js:babel-core/register ./test', (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     done(error);
